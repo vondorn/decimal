018535090e481ec801448c0fedeef5db47ea4215
Grance Tijuana <granceti@in-h6.nsk.21-school.ru>
2024-06-30 19:13:14 +0700

    Удобный коммит

--- src/decimal.c
+++ src/decimal.c
@@ -1,19 +1,23 @@
 #include "decimal.h"
 
+
 int main() {
-  s21_decimal src1, src2;
+  s21_decimal src1;
   src1.bits[0] = 0b00000000000000000000000000000001;
-  src1.bits[1] = 0b01000000000000000000000000000000;
-  src1.bits[2] = 0b00000000000000000000000000000000;
+  src1.bits[1] = 0b00000000000010000000000000000000;
+  src1.bits[2] = 0b10000000000000000000000000000000;
   src1.bits[3] = 0b00000000000000000000000000000000;
-  src2.bits[0] = 0b00000000000000000000000000000001;
-  src2.bits[1] = 0b00000000000000000000000000000000;
-  src2.bits[2] = 0b00000000000000000000000000000000;
-  src2.bits[3] = 0b00000000000000000000000000000000;
-  printf("%d\n", s21_is_less(src1, src2));
-  printf("%d\n", s21_is_greater(src1, src2));
-  // s21_add(src1, src2, &result);
-  // print_decimal(result);
+  // src2.bits[0] = 0b00000000000000000000000011010010;
+  // src2.bits[1] = 0b00000000000000000000000000000001;
+  // src2.bits[2] = 0b00000000000000000000000000000000;
+  // src2.bits[3] = 0b00000000000000000000000000000000;
+  // decimal_normalization(&src1, &src2);
+  // mult_by_num(&src1, 100000000);
+  // mult_by_num(&src1, 100000000);
+  // mult_by_num(&src1, 100000);
+  print_decimal(src1);
+  // s21_is_equal(src1, src2) ? printf("EQUAL") : printf("NO");
+  // printf("%d - %d\n", get_scale(src1), get_scale(src2));
   return 0;
 }
 
@@ -25,12 +29,21 @@ void set_sign(s21_decimal* decimal) {
   decimal->bits[3] |= NEGATIVE;
 }
 
-int get_scale(s21_decimal decimal) { return (decimal.bits[3] >> 16) & SCALE; }
+int get_scale(s21_decimal decimal) { 
+  return (decimal.bits[3] >> 16) & SCALE;
+}
+
+void set_scale(s21_decimal* decimal, int scale) {
+  bool sign = get_sign(*decimal);
+  decimal->bits[3] = scale << 16;
+  if (sign) set_sign(decimal);
+}
 
 void s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
   if (get_sign(value_1) && get_sign(value_2)) {
     set_sign(result);
   }
+  // decimal_normalization(&value_1, &value_2);
   unsigned long long temp = 0;
   for (int i = 0; i < 3; i++) {
     temp += (unsigned long long)value_1.bits[i] + (unsigned long long)value_2.bits[i];
@@ -42,9 +55,8 @@ void s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
 void print_decimal(s21_decimal decimal) {
   if (get_sign(decimal)) printf("-");
   for (int i = 2; i >= 0; i--) {
-    printf("%u", decimal.bits[i]);
+    to_binary(decimal.bits[i]);
     if (!i) printf("\n");
-    else printf(".");
   }
 }
 
@@ -93,6 +105,90 @@ int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
   return !(s21_is_less(value_1, value_2));
 }
 
-int s21_from_decimal_to_int(s21_decimal src, int *dst) {
-  
+// int s21_from_decimal_to_int(s21_decimal src, int *dst) {
+//   for (int i = 2; i >= 0; i--) {
+//     *dst += src.bits[i];
+
+//   }
+// }
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst) {
+  dst->bits[0] = abs(src);
+  for (int i = 1; i < 4; i++) {
+    dst->bits[i] = 0;
+  }
+  dst->bits[3] = src & NEGATIVE;
+  return 0;
+}
+
+int s21_negate(s21_decimal value, s21_decimal* result) {
+  for (int i = 0; i < 3; i++) {
+    result->bits[i] = value.bits[i];
+  }
+  result->bits[3] = (value.bits[3] ^ NEGATIVE);
+  return 0;
+}
+
+void decimal_normalization(s21_decimal* value_1, s21_decimal* value_2) {
+  bool flag_overflow = 0;
+  int scale_1 = get_scale(*value_1);
+  int scale_2 = get_scale(*value_2);
+  while (scale_1 != scale_2) {
+    if (scale_1 > scale_2 && !flag_overflow) {
+      flag_overflow = mult_by_10(value_2, scale_1, &scale_2);
+    } else
+      break;
+  }
+  set_scale(value_2, scale_2);
+}
+
+bool mult_by_10(s21_decimal* decimal, int scale_big, int* scale_little) {
+  bool flag = 0;
+  s21_decimal top_decimal;
+  top_decimal.bits[0] = 0b00011001100110011001100110011001;
+  top_decimal.bits[1] = 0b10011001100110011001100110011001;
+  top_decimal.bits[2] = 0b10011001100110011001100110011001;
+  top_decimal.bits[3] = decimal->bits[3];
+  for (;scale_big > *scale_little; (*scale_little)++) {
+    printf("%d\n", *scale_little);
+    if (s21_is_less_or_equal(*decimal, top_decimal)) {
+      mult_by_num(decimal, 10);
+    } else {
+      flag = 1;
+      break;
+    }
+  }
+  return flag;
+}
+
+void mult_by_num(s21_decimal* decimal, int num) {
+   unsigned long long temp = 0;
+  for (int i = 0; i < 3; i++) { 
+    temp += (unsigned long long)decimal->bits[i] * (unsigned long long)num;
+    decimal->bits[i] = (unsigned)temp;
+    temp >>= 32;
+  }
+}
+
+void to_binary(unsigned n) {
+  int binaryNum[32] = {0};
+    int i = 0;
+    char binaryStr[32] = "";
+
+    while (n > 0) {
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    for (int j = 31; j >= 0; j--) {
+        binaryStr[i - 1 - j] = binaryNum[j] + '0';
+    }
+    char buf[40];
+    sprintf(buf, "%32s", binaryStr);
+    for (int k = 0; k < 32; k++) {
+      if (buf[k] == ' ')
+        buf[k] = '0';
+    }
+    printf("%s", buf);
 }
--- src/decimal.h
+++ src/decimal.h
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #define NEGATIVE 0x80000000
 #define SCALE 0xff
 
 typedef struct {
   unsigned bits[4];
 } s21_decimal;
+
 bool get_sign(s21_decimal decimal);
+void set_sign(s21_decimal* decimal);
 int get_scale(s21_decimal decimal);
+void set_scale(s21_decimal* decimal, int scale);
+void decimal_normalization(s21_decimal* value_1, s21_decimal* value_2);
+
 void s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+
 void print_decimal(s21_decimal decimal);
+
 int s21_is_equal(s21_decimal value_1, s21_decimal value_2);
 int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2);
 int s21_is_less(s21_decimal value_1, s21_decimal value_2);
 int s21_is_greater(s21_decimal value_1, s21_decimal value_2);
 int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2);
 int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2);
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst);
+
+int s21_negate(s21_decimal value, s21_decimal *result);
+
+void mult_by_num(s21_decimal* decimal, int num);
+bool mult_by_10(s21_decimal* decimal, int scale_big, int* scale_little);
+void to_binary(unsigned n);
