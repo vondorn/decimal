5ae228b11c512a5df4c539bbc07f1aa8161b8577
Grance Tijuana <granceti@in-h6.nsk.21-school.ru>
2024-07-02 16:54:34 +0700

    Величесвенный коммит

--- src/decimal.c
+++ src/decimal.c
@@ -1,34 +1,35 @@
 #include "decimal.h"
 
-int main() {
-  s21_decimal src1, src2, result;
+// int main() {
+//   s21_decimal src1, src2, result;
 
-  src1.bits[0] = 0b01011000010000110001100100000110;
-  src1.bits[1] = 0b00000000011100101101011000000101;
-  src1.bits[2] = 0b00000000000000000000000000000000;
-  src1.bits[3] = 0b00000000000000000000000000000000;
-  src2.bits[0] = 0b10111110000100011000010110010111;
-  src2.bits[1] = 0b11011001001111010001001011000110;
-  src2.bits[2] = 0b00000000000001011001011001001000;
-  src2.bits[3] = 0b00000000000100010000000000000000;
-  //  s21_decimal first = {
-  //     {2147483647, 2147483647, 2147483647, 0b10000000000001110000000000000000}};
-  // s21_decimal second = {
-  //     {2147483646, 2147483647, 2147483647, 0b10000000000001110000000000000000}};
-  // print_decimal(src1);
-  // print_decimal(src2);
-  // decimal_normalization(&src1, &src2);
-  // print_decimal(src1);
-  // print_decimal(src2);
-  // s21_add(src1, src2, &result);
-  // // print_decimal(src2);
-  // // printf("%d - %d\n", get_scale(src1), get_scale(src2));
-  // s21_add(src1, src2, &result);
-  // // printf("%d - %d\n", get_scale(src1), get_scale(src2));
-  // print_decimal(result);
-  // printf("%d\n", get_scale(result));
-  return 0;
-}
+//   src1.bits[0] = 0b10010001000010101111010011001010;
+//   src1.bits[1] = 0b11000000010001011101010111110010;
+//   src1.bits[2] = 0b00100111111001000001101100000000;
+//   src1.bits[3] = 0b10000000000101010000000000000000;
+//   src2.bits[0] = 0b00010001110011011101000110000101;
+//   src2.bits[1] = 0b11110101101111000110111111000000;
+//   src2.bits[2] = 0b00000010110101010000111100111111;
+//   src2.bits[3] = 0b10000000000100000000000000000000;
+//   //  s21_decimal first = {
+//   //     {2147483647, 2147483647, 2147483647, 0b10000000000001110000000000000000}};
+//   // s21_decimal second = {
+//   //     {2147483646, 2147483647, 2147483647, 0b10000000000001110000000000000000}};
+//   // print_decimal(src1);
+//   // print_decimal(src2);
+//   // decimal_normalization(&src1, &src2);
+
+//   print_decimal(src1);
+//   print_decimal(src2);
+//   s21_sub(src1, src2, &result);
+//   // // print_decimal(src2);
+//   // // printf("%d - %d\n", get_scale(src1), get_scale(src2));
+//   // s21_add(src1, src2, &result);
+//   // // printf("%d - %d\n", get_scale(src1), get_scale(src2));
+//   print_decimal(result);
+//   // printf("%d\n", get_scale(result));
+//   return 0;
+// }
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
   int flag = 0;
@@ -70,8 +71,12 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
   int sign_1 = get_sign(value_1);
   int sign_2 = get_sign(value_2);
   if (sign_1 && sign_2) {
-    set_sign(result);
-    flag = real_add(value_1, value_2, result);
+    if (s21_is_less_abs(value_1, value_2)){
+      flag = real_sub(value_2, value_1, result);
+    } else {
+      set_sign(result);
+      flag = real_sub(value_1, value_2, result);
+    }
   } else if (!sign_1 && !sign_2) {
     if (s21_is_less_abs(value_1, value_2)){
       set_sign(result);
@@ -106,8 +111,6 @@ void decimal_normalization(s21_decimal* value_1, s21_decimal* value_2) {
   int flag_overflow = 0;
   int scale_1 = get_scale(*value_1);
   int scale_2 = get_scale(*value_2);
-  int overcut[30] = {0};
-  // int i = 0;
   while (scale_1 != scale_2) {
     if (scale_1 > scale_2 && !flag_overflow) {
       flag_overflow = mult_by_10(value_2, scale_1, &scale_2);
@@ -117,11 +120,9 @@ void decimal_normalization(s21_decimal* value_1, s21_decimal* value_2) {
     if (scale_2 > scale_1 && !flag_overflow) {
       flag_overflow = mult_by_10(value_1, scale_2, &scale_1);
     } else if (scale_2 > scale_1 && flag_overflow) {
-      printf("%u", value_2->bits[0]);
       div_by_10(value_2, &scale_2, scale_1);
     }
   }
-  printf("\n%d\n", overcut[0]);
   set_scale(value_2, scale_2);
   set_scale(value_1, scale_1);
 }
