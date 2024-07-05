1db787caeb8746011cc92b9fbb5a2e16c151a2a6
Fred Granules <fredgran@si-b1.nsk.21-school.ru>
2024-07-05 17:38:52 +0700

    additions

--- src/div_v2.c
+++ src/div_v2.c
@@ -15,32 +15,61 @@ int main(){
   s21_long_decimal value_1long, value_2long;
   convert_to_long(value_1, &value_1long);
   convert_to_long(value_2, &value_2long);
-  s21_long_decimal to_use = value_2long;
-  int k = 1, i = -1, j = 0, result = 0;
+  s21_long_decimal to_use1 = value_1long;
+  s21_long_decimal to_use2 = value_2long;
+  double k = 1, i = -1, j = 0;
+   double result = 0;
+   int aaa = 0;
   while(!s21_is_zero_long(value_2long)){
     i = -1, j = 0;
-    copy_long_decimal(&to_use, value_2long);
+    copy_long_decimal(&to_use2, value_2long);
+    copy_long_decimal(&to_use1, value_1long);
+    if (!s21_is_zero_long(value_1long) && !s21_is_less_long(value_1long, value_2long)){
+      while(!s21_is_less_long(value_1long, to_use2)){
+        mult_by_num_long(to_use2, &to_use2, 10);
+        i++;
+      }
+      if (i >= 0) div_by_num_long(&to_use2, 10);
+      s21_long_decimal temp_to_use2 = to_use2;
+      while(!s21_is_less_long(value_1long, to_use2)){
+        real_add_long(to_use2, temp_to_use2, &to_use2);
+        j++;
+      }
+      if (j > 0) real_sub_long(to_use2, temp_to_use2, &to_use2);
+      real_sub_long(value_1long, to_use2, &value_1long);
+      i = pow(10, i);
+      j = j * i;
+      printf("i: %lf\n", i);
+      printf("j: %lf\n", j);
+      result += j;
+    } else if (!s21_is_zero_long(value_1long) && s21_is_less_long(value_1long, value_2long) && aaa < 2){
+      while(s21_is_less_long(to_use1, value_2long)){
+        mult_by_num_long(to_use1, &to_use1, 10);
+        // i++;
+      }
+      while(!s21_is_less_long(to_use1, to_use2)){
+        mult_by_num_long(to_use2, &to_use2, 10);
+        i--;
+      }
+      if (i >= 0) div_by_num_long(&to_use2, 10);
+      s21_long_decimal temp_to_use2 = to_use2;
+      while(!s21_is_less_long(to_use1, to_use2)){
+        real_add_long(to_use2, temp_to_use2, &to_use2);
+        j++;
+      }
+      if (j > 0) real_sub_long(to_use2, temp_to_use2, &to_use2);
+      real_sub_long(to_use1, to_use2, &to_use1);
 
-    if (s21_is_zero_long(value_1long) || s21_is_less_long(value_1long, value_2long)){
-      break;
-    }
-    while(!s21_is_less_long(value_1long, to_use)){
-      mult_by_num_long(to_use, &to_use, 10);
-      i++;
-    }
-    if (i >= 0) div_by_num_long(&to_use, 10);
-    s21_long_decimal temp_to_use = to_use;
-    while(!s21_is_less_long(value_1long, to_use)){
-      real_add_long(to_use, temp_to_use, &to_use);
-      j++;
-    }
-    real_sub_long(to_use, temp_to_use, &to_use);
-    real_sub_long(value_1long, to_use, &value_1long);
-    i = pow(10, i);
-    j = j * i;
-    printf("i: %d\n", i);
-    printf("j: %d\n", j);
-    result += j;
+      print_long_decimal(to_use2);
+      print_long_decimal(to_use1);
+      // if (i == 0) i = 0.1;
+      i = pow(10, i);
+      j = j * i;
+      printf("-i: %lf\n", i);
+      printf("-j: %lf\n", j);
+      result +=j;
+      aaa++;
+    } else break;
   }
-  printf("result: %d", result);
+  printf("result: %lf", result);
 }
