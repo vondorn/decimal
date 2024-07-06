c9f6f3f3c25b683c0297e23ba04f857f89642ced
Fred Granules <fredgran@in-h4.nsk.21-school.ru>
2024-07-06 16:30:06 +0700

    div working

--- src/div_v2.c
+++ src/div_v2.c
@@ -19,7 +19,7 @@ int main(){
   s21_long_decimal to_use2 = value_2long;
   double k = 1, i = -1, j = 0;
    double result = 0;
-   int aaa = 0;
+   int aaa = 1;
   while(!s21_is_zero_long(value_2long)){
     i = -1, j = 0;
     copy_long_decimal(&to_use2, value_2long);
@@ -42,7 +42,7 @@ int main(){
       printf("i: %lf\n", i);
       printf("j: %lf\n", j);
       result += j;
-    } else if (!s21_is_zero_long(value_1long) && s21_is_less_long(value_1long, value_2long) && aaa < 2){
+    } else if (!s21_is_zero_long(value_1long) && s21_is_less_long(value_1long, value_2long) && aaa < 10){
       while(s21_is_less_long(to_use1, value_2long)){
         mult_by_num_long(to_use1, &to_use1, 10);
         // i++;
@@ -51,7 +51,9 @@ int main(){
         mult_by_num_long(to_use2, &to_use2, 10);
         i--;
       }
-      if (i >= 0) div_by_num_long(&to_use2, 10);
+      print_long_decimal(to_use2);
+      if (i <= 0) div_by_num_long(&to_use2, 10);
+      print_long_decimal(to_use1);
       s21_long_decimal temp_to_use2 = to_use2;
       while(!s21_is_less_long(to_use1, to_use2)){
         real_add_long(to_use2, temp_to_use2, &to_use2);
@@ -59,15 +61,13 @@ int main(){
       }
       if (j > 0) real_sub_long(to_use2, temp_to_use2, &to_use2);
       real_sub_long(to_use1, to_use2, &to_use1);
-
-      print_long_decimal(to_use2);
-      print_long_decimal(to_use1);
-      // if (i == 0) i = 0.1;
-      i = pow(10, i);
-      j = j * i;
+      if (i == 0) i *= 0.1;
+      copy_long_decimal(&value_1long, to_use1);
+      i = pow(10, -aaa);
       printf("-i: %lf\n", i);
+      j = j * i;
+      result += j;
       printf("-j: %lf\n", j);
-      result +=j;
       aaa++;
     } else break;
   }
